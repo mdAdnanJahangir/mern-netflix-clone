2810fe28925ea83c7d1966cbb3a84f50921fd45a
adnan <adnan.dev457@gmail.com>
2025-09-20 18:52:26 +0530

    movie and tv controllers

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 import express from "express"
 
 import authRoutes from "./routes/auth.route.js"
+import movieRoutes from "./routes/movie.route.js"
+import tvRoutes from "./routes/tv.route.js"
+
+
+
 import { ENV_VARS } from "./config/envVars.js"
 import { connectDB } from "./config/db.js"
 
@@ -15,6 +20,8 @@ app.use(express.json())
 
 
 app.use("/api/v1/auth", authRoutes)
+app.use("/api/v1/movie", movieRoutes)
+app.use("/api/v1/tv", tvRoutes)
 
 
 
@@ -26,4 +33,6 @@ app.listen(PORT,()=>{
     console.log(`server started at http://localhost:${PORT}`);
      connectDB()
 })
+
+
 
