28d1f98d976467cc99d783af14cdd25f52380c01
adnan <mdajahangir2005@gmail.com>
2025-09-20 18:52:26 +0530

    movie and tv controllers

---
+++ backend/services/tmdb.service.js
@@ -0,0 +1,27 @@
+
+//const url = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
+import axios from "axios"
+import { ENV_VARS } from "../config/envVars.js";
+
+export const fetchFromTMDB = async (url) =>{
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer '+ENV_VARS.TMDB_API_KEY,
+  }
+};
+
+const responce =await axios.get(url, options)
+
+
+if(responce.status!==200){
+    throw new Error("Failed to fetch data from TMDB"+responce.status)
+}
+
+return responce.data
+
+  }
+
+
