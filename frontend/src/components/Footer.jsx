a03f147f9fb66a13c1599af41e7d3c8085f239ea
adnan <mdajahangir2005@gmail.com>
2025-10-31 00:12:33 +0530

    updated footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-
 const Footer = () => {
-  return (
-    <footer>Footer component</footer>
-  )
-}
-
-export default Footer
+	return (
+		<footer className='py-6 md:px-8 md:py-0 bg-black text-white border-t border-gray-800'>
+			<div className='flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row'>
+				<p className='text-balance text-center text-sm leading-loose text-muted-foreground md:text-left'>
+					Built by{" MD ADNAN"}
+					<a
+						href='https://github.com/mdAdnanJahangir'
+						target='_blank'
+						className='font-medium underline underline-offset-4'
+					>
+						you
+					</a>
+					. The source code is available on{"MD ADNAN GITHUB "}
+					<a
+						href='https://github.com/mdAdnanJahangir'
+						target='_blank'
+						rel='noreferrer'
+						className='font-medium underline underline-offset-4'
+					>
+						GitHub
+					</a>
+					.
+				</p>
+			</div>
+		</footer>
+	);
+};
+export default Footer;
